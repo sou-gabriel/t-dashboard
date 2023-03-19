82a9beacd688c10b11da5b377b3b5aaee40df3bb
Gabriel Ramos <dev.gabrielramos@gmail.com>
2023-03-19 15:27:52 -0300

    chore: create initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Assets from './pages/Assets';
+
+export default function App() {
+  return (
+    <div>
+      <Assets />
+    </div>
+  );
+}
---
+++ src/pages/Assets/index.tsx
@@ -0,0 +1,5 @@
+export default function Assets() {
+  return (
+    <h1>AssetsPage</h1>
+  );
+}
